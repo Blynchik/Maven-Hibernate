55b12afa9daf9beb02fb4695e687396ab0e3d427
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-04-19 17:41:13 +0300

    5.4

--- src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/service/EmployeeServiceImpl.java
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/service/EmployeeServiceImpl.java
@@ -19,4 +19,10 @@ public class EmployeeServiceImpl implements EmployeeService {
     public List<Employee> getAllEmployees() {
         return employeeDAO.getAllEmployees();
     }
+
+    @Transactional
+    @Override
+    public void saveEmployee(Employee employee) {
+        employeeDAO.saveEmployee(employee);
+    }
 }
